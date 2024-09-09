60f9974e174145d1e0172a3b265f1a8a209c25b2
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-08 17:56:38 -0700

    :art: Add some better types.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,23 +1,23 @@
 import { Plugin } from "rollup"
 
-export type Watch =
+export type Watcher =
   | {
       pattern: RegExp | string
       action: (match: RegExpMatchArray, id: string) => string[]
     }
-  | [RegExp | string, (match: RegExpMatchArray, id: string) => string[]]
+  | [pattern: RegExp | string, action: (match: RegExpMatchArray, id: string) => string[]]
 
-export function guardLikeWatch(watches: Watch[]): Plugin {
+export function guardLikeWatch(watchers: Watcher[]): Plugin {
   return {
     name: "vite-plugin-guard-like-watch",
     transform(_code, id) {
-      for (const watch of watches) {
-        const pattern = Array.isArray(watch) ? watch[0] : watch.pattern
+      for (const watcher of watchers) {
+        const pattern = Array.isArray(watcher) ? watcher[0] : watcher.pattern
         const regexp = new RegExp(pattern)
         const match = id.match(regexp)
         if (match === null || match === undefined) return
 
-        const action = Array.isArray(watch) ? watch[1] : watch.action
+        const action = Array.isArray(watcher) ? watcher[1] : watcher.action
         const filesToWatch = action(match, id)
         for (const file of filesToWatch) {
           this.addWatchFile(file)
